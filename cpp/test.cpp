70c9f0c3ea35476e09b66cc103aabc88038d8f13
ZachDaChampion <zachchampion79@gmail.com>
2024-01-25 23:49:39 -0500

    Refactor SimpleBufferWriter classes and update test.cpp

--- cpp/test.cpp
+++ cpp/test.cpp
@@ -27,41 +27,46 @@ class YWriter;
 
 class XWriter : public SimpleBufferWriter {
    public:
-    uint16_t size() const override { return 7; }
     XWriter(uint8_t a, char* b, ArrayWriter<YWriter> c) : a(a), b(b), c(c) {}
 
     uint8_t a;
     char* b;
     ArrayWriter<YWriter> c;
 
+    uint16_t static_size() const override { return 5; }
+
     uint8_t* write_component(uint8_t* dest, const uint8_t* dest_end,
-                             uint8_t* dyn_cursor = nullptr) const override {
+                             uint8_t* dyn_cursor) const override {
         if (dest_end - dest < 7) return nullptr;
-        if (dyn_cursor == nullptr) dyn_cursor = dest + 7;
         dyn_cursor = write_field(dest, dest_end, dyn_cursor, a);
+        if (dyn_cursor == nullptr) return nullptr;
         dest += get_static_size(a);
         dyn_cursor = write_field(dest, dest_end, dyn_cursor, b);
+        if (dyn_cursor == nullptr) return nullptr;
         dest += get_static_size(b);
         dyn_cursor = write_field(dest, dest_end, dyn_cursor, c);
+        if (dyn_cursor == nullptr) return nullptr;
         return dyn_cursor;
     }
 };
 
 class YWriter : public SimpleBufferWriter {
    public:
-    uint16_t size() const override { return 3; }
     YWriter(uint8_t d, char* e) : d(d), e(e) {}
 
     uint8_t d;
     char* e;
 
+    uint16_t static_size() const override { return 3; }
+
     uint8_t* write_component(uint8_t* dest, const uint8_t* dest_end,
-                             uint8_t* dyn_cursor = nullptr) const override {
+                             uint8_t* dyn_cursor) const override {
         if (dest_end - dest < 2) return nullptr;
-        if (dyn_cursor == nullptr) dyn_cursor = dest + 2;
         dyn_cursor = write_field(dest, dest_end, dyn_cursor, d);
+        if (dyn_cursor == nullptr) return nullptr;
         dest += get_static_size(d);
         dyn_cursor = write_field(dest, dest_end, dyn_cursor, e);
+        if (dyn_cursor == nullptr) return nullptr;
         return dyn_cursor;
     }
 };
@@ -70,7 +75,7 @@ uint8_t buffer[100];
 
 int main() {
     YWriter c_writers[] = {YWriter(1, "c1"), YWriter(2, "c2")};
-    XWriter x_writer(5, "b str", ArrayWriter(c_writers, 2));
+    XWriter x_writer(5, "b str", ArrayWriter<YWriter>(c_writers, 2));
 
     int32_t bytes_written = x_writer.write(buffer, 100);
 
