564b333b00103976aac2f936a2bec2eb85183f79
Zach Champion <zachchampion79@gmail.com>
2024-07-08 18:46:33 -0400

    Begin adding cpp codegen

--- simplebuffers-compiler/src/internal_generators.rs
+++ simplebuffers-compiler/src/internal_generators.rs
@@ -1,18 +1,20 @@
 use simplebuffers_codegen::CodeGenerator;
 
 macro_rules! register_internal_generators {
-    ($($name:literal : $generator:ty);*) => {
+    ($($name:literal : $generator:ty),*) => {
         pub(crate) fn get_internal_generator(name: &str) -> Option<Box<dyn CodeGenerator>> {
             match (name) {
-            $(
-                $name => Some(Box::new(<$generator as CodeGenerator>::new())),
-            );*
-            _ => None
+                $(
+                    $name => Some(Box::new(<$generator as CodeGenerator>::new())),
+                )*
+                _ => None
+            }
         }
-    }
     };
 }
 
 register_internal_generators!(
-    "sanitycheck": simplebuffers_sanitycheck::SanityCheckCodeGenerator
+    "sanitycheck": simplebuffers_sanitycheck::SanityCheckCodeGenerator,
+    "c++": simplebuffers_cpp::CPPCodeGenerator,
+    "cpp": simplebuffers_cpp::CPPCodeGenerator
 );
