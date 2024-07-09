ee4ed3d845bd4d5f0752bccaa7645fe5688f588d
Zach Champion <zachchampion79@gmail.com>
2024-07-08 22:11:54 -0400

    Fix docs

--- copy-docs.ps1
+++ copy-docs.ps1
@@ -1,5 +1,5 @@
 rm ./target/doc -r -fo
 cargo doc --no-deps
-echo '<meta http-equiv=\"refresh\" content=\"0; url=simplebuffers_core\">' > target/doc/index.html
+echo '<meta http-equiv="refresh" content="0; url=simplebuffers_core">' > target/doc/index.html
 rm ./docs -r -fo
 cp -r target/doc ./docs
